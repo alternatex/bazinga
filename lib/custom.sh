7374b047be28a9a115ef31d5102b00f3aa616e4d
alternatex <gianni.furger@gmail.com>
2013-01-25 16:53:58 +0100

    started common/custom base for ext inclusion

---
+++ lib/custom.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# custom configuration » TODO: include file in all common projects. abstract lil' more.
+bazinga_configure_custom() {
+
+  # internals
+  BAZINGA_HOME="`dirname $1`/.." && cd $BAZINGA_HOME
+  BAZINGA_INSTALL="`which bazinga 2>&1`"
+  BAZINGA_INSTALL_DIR="`dirname $BAZINGA_INSTALL 2>&1`/.."
+
+  # include core
+  source $BAZINGA_INSTALL_DIR/lib/bazinga.sh
+
+  # intialize / gather custom configuration
+  bazinga_init $bazinga_namespace
+
+  # shizzl. don't look. todo » solve overwrite data-loss » could be more sexy, right?
+  bazinga_custom="${bazinga_custom}.tmp"
+
+  # gather custom configuration
+  bazinga_gather
+
+  # replace configurations - write new configuration to disk (actually replacing conf w/ temporary one)
+  bazinga_flush
+
+  # dynamic export / convert environment variables as json w/ php-helper
+  php $BAZINGA_INSTALL_DIR/mod/json/json.php $bazinga_namespace $bazinga_custom_json
+}
